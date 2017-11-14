7106d0af8b8c956ed9a870db5ab10e3eade9dc49
lom360 <lom360@mail.fresnostate.edu>
2017-11-13 23:25:55 -0800

    Added class to test out comments

---
+++ Chapter1/src/CommentDemo.java
@@ -0,0 +1,10 @@
+/*
+ * Created by Lom on 11/13/2017
+ */
+
+public class CommentDemo {
+    public static void main(String[] args){
+        System.out.println("Hello world"); // this is also a comment
+        System.out.println("My name is Lom"); // comments gets ignored by the compiler.
+    }
+}
