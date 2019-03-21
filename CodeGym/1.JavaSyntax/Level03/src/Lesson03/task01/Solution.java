ef0c624db5ec6beb19b6c4456d7af6e672e8d7c4
Lom Thin <sarom_thin@outlook.com>
2019-03-20 23:50:15 -0700

    Completed lesson 01 of level 3

---
+++ CodeGym/1.JavaSyntax/Level03/src/Lesson03/task01/Solution.java
@@ -0,0 +1,15 @@
+package Lesson03.task01;
+
+/* 
+Display right away
+
+*/
+public class Solution {
+    public static void printString(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
