664fe005fbb4ad9d9ce163c4db83ab302f7b307f
Lom Thin <sarom_thin@outlook.com>
2019-03-22 22:47:06 -0700

    Completed paranthesis brainteaser

---
+++ CodeGym/1.JavaSyntax/Level03/src/Lesson04/task02/Solution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task03.task0306;
+
+/* 
+Parenthetical brainteaser
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // The line below was how the problem is set.
+        // System.out.println((2 * 3) + 4 * 5 + (6 * 7));
+        System.out.println(2 * (3 + 4 * (5 + 6 * 7)));
+    }
+}
