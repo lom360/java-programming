54dba470b753bc39d5e3c9c5c3c99691d6e77b75
Lom Thin <sarom_thin@outlook.com>
2019-04-04 01:17:46 -0700

    Completed task 03 of lesson 12

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task03/task0325/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task03.task0325;
+
+import java.io.*;
+
+/* 
+Financial expectations
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sNum = reader.readLine();
+        int n = Integer.parseInt(sNum);
+
+        System.out.println("I will earn $" + n + " per hour");
+    }
+    }
+}
