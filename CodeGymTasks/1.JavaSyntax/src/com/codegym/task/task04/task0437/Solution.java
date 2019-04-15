60ab4c84f8e418a80c5a879d5ea3d28288fa152a
Lom Thin <sarom_thin@outlook.com>
2019-04-15 00:43:03 -0700

    Changed task0437

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task04/task0437/Solution.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task04/task0437/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         //write your code here
         for(int i = 1; i <= 10; i++) {
-            for(int j = i; j > 0; j--) {
+            for(int j = 1; j <= i; j++) {
                 System.out.print(8);
             }
             System.out.println();
