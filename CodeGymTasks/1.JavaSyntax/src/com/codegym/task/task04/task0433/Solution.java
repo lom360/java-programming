a5ebade5e029d6ff11f135159fdc730d5883ba11
Lom Thin <sarom_thin@outlook.com>
2019-04-14 23:30:29 -0700

    Completed task0433

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task04/task0433/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task04.task0433;
+
+
+/* 
+Seeing dollars in your future
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        int num = 10;
+
+        while(num > 0) {
+            System.out.println("$$$$$$$$$$");
+            num--;
+        }
+    }
+}
