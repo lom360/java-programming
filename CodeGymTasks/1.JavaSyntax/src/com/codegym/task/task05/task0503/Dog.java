e3b45bac235866ac5f17bf09aaf14feace7c8f54
Lom Thin <sarom_thin@outlook.com>
2019-04-18 10:53:05 -0700

    Completed task0503

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task05/task0503/Dog.java
@@ -0,0 +1,34 @@
+package com.codegym.task.task05.task0503;
+
+
+/* 
+Getters and setters for the Dog class
+
+*/
+
+public class Dog {
+    //write your code here
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
