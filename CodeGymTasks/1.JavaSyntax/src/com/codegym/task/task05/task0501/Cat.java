085314d5ab89f8fc663dac71fd60a4ad74bf415a
Lom Thin <sarom_thin@outlook.com>
2019-04-18 10:10:48 -0700

    Completed task0501

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task05/task0501/Cat.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task05/task0501/Cat.java
@@ -8,7 +8,12 @@ Creating a cat
 public class Cat {
     //write your code here
 
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
     public static void main(String[] args) {
-        System.out.println("Testing");
+
     }
 }
