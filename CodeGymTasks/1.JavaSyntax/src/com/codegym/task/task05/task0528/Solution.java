bf1afccf360b3485ea8cdcf524e4fe8c9c9ee059
Lom Thin <sarom_thin@outlook.com>
2019-04-25 23:17:44 -0700

    Completed task0528

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task05/task0528/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task05.task0528;
+
+/* 
+Display today's date
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+        String month = "04";
+        String day = "26";
+        String year = "2019";
+
+        System.out.println(month + " " + day + " " + year);
+    }
+}
