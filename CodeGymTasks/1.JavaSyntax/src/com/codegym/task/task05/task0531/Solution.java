b98ae545b4cc06f7e415f70cc816018f8ebbb239
Lom Thin <sarom_thin@outlook.com>
2019-04-25 23:50:25 -0700

    Completed task0531

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task05/task0531/Solution.java
@@ -0,0 +1,44 @@
+package com.codegym.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Improving functionality
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b, c, d, e);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b, int c, int d, int e) {
+        int arr[] = new int[5];
+        arr[0] = a;
+        arr[1] = b;
+        arr[2] = c;
+        arr[3] = d;
+        arr[4] = e;
+
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
