7c89be2d3635cd75ce17a0e1428de098cbfa0eb1
Lom Thin <sarom_thin@outlook.com>
2019-05-02 00:03:09 -0700

    Completed task0606

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task06/task0607/Cat.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task06.task0607;
+
+/* 
+Class counter
+
+*/
+
+public class Cat {
+    //write your code here
+    static int catCount;
+
+    public Cat() {
+        this.catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
