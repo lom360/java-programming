f98378a157915f86b97ffaaa36859b8bc7f985dd
Lom Thin <sarom_thin@outlook.com>
2019-05-03 20:28:26 -0700

    Completed task0618

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task06/task0618/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task06.task0618;
+
+/* 
+KissMyShinyMetalRearActuator
+
+*/
+
+public class Solution {
+    public static class KissMyShinyMetalRearActuator {
+
+    }
+
+    public static void main(String[] args) {
+        //write your code here
+        KissMyShinyMetalRearActuator robot = new KissMyShinyMetalRearActuator();
+
+        System.out.println(robot);
+    }
+}
