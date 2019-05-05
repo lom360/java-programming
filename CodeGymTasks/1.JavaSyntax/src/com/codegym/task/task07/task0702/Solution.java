420defb850b1659566dd381bdbf9a06c37f03468
Lom Thin <sarom_thin@outlook.com>
2019-05-05 00:28:34 -0700

    Completed task0701

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task07/task0702/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task07.task0702;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+String array in reverse order
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        String[] arr = new String[10];
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
