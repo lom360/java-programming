e38fb9d136c38637e05c76405b21aac76c7d8c6d
Lom Thin <sarom_thin@outlook.com>
2019-05-08 19:24:34 -0700

    Completed task0708

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task07/task0708/Solution.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task07/task0708/Solution.java
@@ -11,27 +11,27 @@ Longest string
 */
 
 public class Solution {
-    private static List<String> strings;
+    private static List<String> strings = new ArrayList<String>() {
+    };
 
     public static void main(String[] args) throws Exception {
         //write your code here
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<String>();
 
         for(int i = 0; i < 5; i++) {
-            list.add(reader.readLine());
+            strings.add(reader.readLine());
         }
 
-        int shortestString = list.get(0).length(); // Start with first item in array.
-        for(int i = 1; i < list.size(); i++) {
-            if(shortestString > list.get(i).length()) {
-                shortestString = list.get(i).length();
+        int longestString = strings.get(0).length(); // Start with first item in array.
+        for(int i = 1; i < strings.size(); i++) {
+            if(longestString < strings.get(i).length()) {
+                longestString = strings.get(i).length();
             }
         }
 
-        for(int i = 0; i < list.size(); i++) {
-            if(shortestString == list.get(i).length()) {
-                System.out.println(list.get(i));
+        for(int i = 0; i < strings.size(); i++) {
+            if(longestString == strings.get(i).length()) {
+                System.out.println(strings.get(i));
             }
         }
     }
