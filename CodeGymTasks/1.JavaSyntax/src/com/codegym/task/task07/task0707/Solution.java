c58f2a754e4767d2ca4315f3db8926d1114eef36
Lom Thin <sarom_thin@outlook.com>
2019-05-08 18:32:52 -0700

    Completed task0707

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task07/task0707/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task07.task0707;
+
+import java.util.ArrayList;
+
+/* 
+What sort of list is that?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("Green");
+        list.add("Blue");
+        list.add("Red");
+        list.add("Yellow");
+        list.add("Black");
+
+        System.out.println(list.size());
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
