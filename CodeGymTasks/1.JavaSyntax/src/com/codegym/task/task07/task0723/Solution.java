1ae6d10e45eb95e0e5380beaf29e1c4bffd92ada
Lom Thin <sarom_thin@outlook.com>
2019-05-11 22:10:53 -0700

    Completed task0723

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task07/task0723/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task07.task0723;
+
+/* 
+Countdown
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+
+            //write your code here
+            try{
+                Thread.sleep(100);
+            }
+            catch(InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+
+        System.out.println("Boom!");
+    }
+}
