1f4bf5675211ef3c2cd23d3e939e1dff7765fed0
Lom Thin <sarom_thin@outlook.com>
2019-05-16 19:53:28 -0700

    Completed task0813

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task03/task0307/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task03.task0307;
+
+/* 
+Hello, StarCraft!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+    }
+
+    public static class Zerg {
+        public String name;
+    }
+
+    public static class Protoss {
+        public String name;
+    }
+
+    public static class Terran {
+        public String name;
+    }
+}
