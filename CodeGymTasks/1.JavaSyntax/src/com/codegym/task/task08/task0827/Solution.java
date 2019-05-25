06bba41f85857b5668e3373e55f942a89f931d19
Lom Thin <sarom_thin@outlook.com>
2019-05-24 23:45:02 -0700

    Working on task0827

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task08/task0827/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task08.task0827;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/* 
+Working with dates
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("JANUARY 2 2020"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        Date currentTime = new Date(date); // This represents date since the beginning of the year.
+
+        Date yearStartTime = new Date(); // This will store the start of the year.
+        yearStartTime.setHours(0);
+        yearStartTime.setMinutes(0);
+        yearStartTime.setSeconds(0);
+        yearStartTime.setDate(1);  // Setting to the first day of the month.
+        yearStartTime.setMonth(0); // Setting month to January.
+        yearStartTime.setYear(currentTime.getYear() + 1900); // Must be the same year of the argument that was passed.
+
+        long msTimeDifference = currentTime.getTime() - yearStartTime.getTime(); // Gets the number of days.
+        long msDay = 24 * 60 * 60 * 1000; // The number of milliseconds in 24 hours.
+
+        int dayCount = (int) (msTimeDifference/msDay); // The number of full days.
+
+
+        if(dayCount % 2 != 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
