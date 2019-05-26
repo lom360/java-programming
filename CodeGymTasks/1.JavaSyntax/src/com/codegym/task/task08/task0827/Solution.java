b61a31ece67b3acb25404830331353058e3164dc
Lom Thin <sarom_thin@outlook.com>
2019-05-25 22:41:14 -0700

    Completed task0827

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task08/task0827/Solution.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task08/task0827/Solution.java
@@ -13,7 +13,7 @@ Working with dates
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(isDateOdd("JANUARY 2 2020"));
+        System.out.println(isDateOdd("JANUARY 1 2020"));
     }
 
     public static boolean isDateOdd(String date) {
@@ -25,15 +25,18 @@ public class Solution {
         yearStartTime.setSeconds(0);
         yearStartTime.setDate(1);  // Setting to the first day of the month.
         yearStartTime.setMonth(0); // Setting month to January.
-        yearStartTime.setYear(currentTime.getYear() + 1900); // Must be the same year of the argument that was passed.
+        yearStartTime.setYear(currentTime.getYear()); // Must be the same year of the argument that was passed.
 
         long msTimeDifference = currentTime.getTime() - yearStartTime.getTime(); // Gets the number of days.
         long msDay = 24 * 60 * 60 * 1000; // The number of milliseconds in 24 hours.
 
-        int dayCount = (int) (msTimeDifference/msDay); // The number of full days.
-
+        int dayCount = (int) (msTimeDifference/msDay);; // The number of full days.
 
         if(dayCount % 2 != 0) {
+            return true;
+        }
+        // This branch statement is for the condition of when currentTime is January 1.
+        else if(currentTime.compareTo(yearStartTime) == -1) {
             return true;
         }
         else {
