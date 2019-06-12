3dde7318e09c2c8ed5aa0d0206b2de4203130000
Lom Thin <sarom_thin@outlook.com>
2019-06-11 22:34:16 -0700

    Completed task0917

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task09/task0916/Solution.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task09/task0916/Solution.java
@@ -5,7 +5,18 @@ import java.rmi.RemoteException;
 
 /* 
 Catching checked exceptions
+In the handleExceptions method, handle all checked exceptions.
+You need to display the checked exception that occurs.
+You can only use one try-catch block.
 
+
+Requirements:
+1. The handleExceptions method must call method1.
+2. The handleExceptions method must call method2.
+3. The handleExceptions method must call method3.
+4. The handleExceptions method must use only one try-catch block.
+5. The handleExceptions method should catch and display all the exceptions that occur.
+6. The program should display text.
 */
 
 public class Solution {
@@ -15,9 +26,14 @@ public class Solution {
     }
 
     public static void handleExceptions(Solution obj) {
-        obj.method1();
-        obj.method2();
-        obj.method3();
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 
     public void method1() throws IOException {
