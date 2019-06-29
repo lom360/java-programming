1da338736853861a9ff47b99a57e21bf6ad08205
Lom Thin <sarom_thin@outlook.com>
2019-06-29 00:42:00 -0700

    Completed task0926

--- CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task09/task0926/Solution.java
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task09/task0926/Solution.java
@@ -26,7 +26,24 @@ public class Solution {
 
     public static ArrayList<int[]> createList() {
         //write your code here
-        
+        ArrayList<int[]> newList = new ArrayList<int[]>();
+
+
+        int[] list0 = new int[5];
+        int[] list1 = new int[2];
+        int[] list2 = new int[4];
+        int[] list3 = new int[7];
+        int[] list4 = new int[0];
+
+        // Call the FillArray method to fill the arrays.
+        newList.add(FillArray(list0));
+        newList.add(FillArray(list1));
+        newList.add(FillArray(list2));
+        newList.add(FillArray(list3));
+        newList.add(FillArray(list4));
+
+
+        return newList;
     }
 
     public static void printList(ArrayList<int[]> list) {
@@ -35,5 +52,14 @@ public class Solution {
                 System.out.println(x);
             }
         }
+    }
+
+    // For this task. I'm making it simple and just adding number 1 to n.
+    public static int[] FillArray(int[] list) {
+        for(int i = 0; i < list.length; i++) {
+            list[i] = i + 1;
+        }
+
+        return list;
     }
 }
