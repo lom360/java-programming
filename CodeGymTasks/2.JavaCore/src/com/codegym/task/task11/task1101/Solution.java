aeecf949d9047e76dd630a3cddb59c5ac0289c14
Lom Thin <sarom_thin@outlook.com>
2019-08-19 21:26:18 -0700

    Completed task1101

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task11/task1101/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task11.task1101;
+
+/* 
+Horse and Pegasus
+Edit the following two classes: Horse and Pegasus.
+Make the Pegasus class inherit the Horse class.
+
+Requirements:
+1. The Solution class must contain the Horse class.
+2. The Horse class should be public.
+3. The Solution class must contain the Pegasus class.
+4. The Pegasus class should be public.
+5. The Pegasus class should inherit the Horse class.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse{
+
+    }
+}
