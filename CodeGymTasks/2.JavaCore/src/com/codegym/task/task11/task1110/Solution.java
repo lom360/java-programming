a1ee449457eb6b52230e4c00c32eaf904181931d
Lom Thin <sarom_thin@outlook.com>
2019-08-19 22:31:01 -0700

    Complete task1110

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task11/task1110/Solution.java
@@ -0,0 +1,46 @@
+package com.codegym.task.task11.task1110;
+
+/* 
+Don't forget to encapsulate
+Look carefully at the methods and add the missing fields.
+
+Requirements:
+1. The Cat class must have a String field name.
+2. The Cat class must have an int field age.
+3. The Cat class must have an int field weight.
+4. The Cat class must have an int field speed.
+5. The Cat class must have 4 fields.
+6. All of the Cat class's fields must be private.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
