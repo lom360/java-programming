88cd82105ec133b5d4bc7c7a4dd1e67991ed8c04
Lom Thin <sarom_thin@outlook.com>
2019-08-20 17:15:44 -0700

    Completed task1116

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task11/task1116/Solution.java
@@ -0,0 +1,30 @@
+package com.codegym.task.task11.task1116;
+
+/* 
+Inheritance chain
+Create a proper "inheritance chain" for the following classes: Pet, Cat, and Dog.
+
+Requirements:
+1. The Solution class must contain a public Pet class.
+2. The Solution class must contain a public Cat class.
+3. The Solution class must contain a public Dog class.
+4. The Cat class should inherit the Pet class.
+5. The Dog class should inherit the Pet class.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Dog extends Pet{
+
+    }
+}
