45bddde91ea7b6434c5b1f7e7f710c9fc9cc4c33
Lom Thin <sarom_thin@outlook.com>
2019-08-20 17:10:30 -0700

    Completed task1114

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task11/task1114/Solution.java
@@ -0,0 +1,31 @@
+package com.codegym.task.task11.task1114;
+
+/* 
+Evolving classes
+Edit the following three classes: Fish, Lizard, and Dinosaur.
+Make Lizard inherit Fish, and Dinosaur inherit Lizard.
+
+Requirements:
+1. The Solution class must contain a public Fish class.
+2. The Solution class must contain a public Lizard class.
+3. The Solution class must contain a public Dinosaur class.
+4. The Lizard class must inherit the Fish class.
+5. The Dinosaur class must inherit the Lizard class.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish{
+
+    }
+
+    public class Dinosaur extends Lizard{
+
+    }
+}
