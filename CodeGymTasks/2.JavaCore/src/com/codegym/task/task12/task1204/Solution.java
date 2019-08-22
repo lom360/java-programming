45ba0f80dd527b5db5217000e299076c2df6ac02
Lom Thin <sarom_thin@outlook.com>
2019-08-21 21:57:01 -0700

    Completed task1204

--- CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1204/Solution.java
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1204/Solution.java
@@ -2,7 +2,15 @@ package com.codegym.task.task12.task1204;
 
 /* 
 Whether it's a bird or a lamp
+Write a method which determines the class type of the object you pass into it, and then displays on the screen the corresponding message: Cat, Dog, Bird, and Lamp.
 
+Requirements:
+1. The program must display text on the screen.
+2. Don't change the Cat class.
+3. Don't change the Dog class.
+4. Don't change the Bird class.
+5. Don't change the Lamp class.
+6. The printObjectType() method must display one of the following messages, depending on the object passed to it. For example, it should display "Lamp" for a Solution.Lamp object.
 */
 
 public class Solution {
@@ -16,6 +24,7 @@ public class Solution {
 
     public static void printObjectType(Object o) {
         //write your code here
+        System.out.println(o.getClass().getSimpleName());
     }
 
     public static class Cat {
