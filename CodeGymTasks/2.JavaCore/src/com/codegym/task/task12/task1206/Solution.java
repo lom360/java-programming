9f2734d0d94e7facaa935d3efbf58c2492a0f26e
Lom Thin <sarom_thin@outlook.com>
2019-08-22 16:56:16 -0700

    Completed task1206

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1206/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task12.task1206;
+
+/* 
+We're on overload!
+Write the following two methods: print(int) and print(String).
+
+Requirements:
+1. The program should not display text on the screen.
+2. The Solution class must contain three methods.
+3. The Solution class must contain a static main() method.
+4. The Solution class must contain a static print() method with an int parameter.
+5. The Solution class must contain a static print() method with a String parameter.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print(int n) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+}
