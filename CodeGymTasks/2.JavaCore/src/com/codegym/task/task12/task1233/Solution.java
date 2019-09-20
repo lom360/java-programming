5027a4468826a11ea739331dada73dc77af953ac
Lom Thin <sarom_thin@outlook.com>
2019-09-19 21:28:01 -0700

    Completed task1232

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1233/Solution.java
@@ -0,0 +1,50 @@
+package com.codegym.task.task12.task1233;
+
+/* 
+The isomorphs are coming
+Write a method that returns the minimum of an array and its position (index).
+
+Requirements:
+1. The Solution class must have a Pair class.
+2. The Solution class must have two methods.
+3. The Solution class must have a getMinimumAndIndex() method.
+4. The getMinimumAndIndex() method must return the minimum of the array and its position (index).
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The index of the minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //write your code here
+        int min = 0; // We will store the index of min, not the value. And we'll start from index 0.
+        for(int i = 1; i < array.length; i++) {
+            if(array[min] > array[i]) {
+                min = i;
+            }
+        }
+
+        return new Pair<Integer, Integer>(array[min], min);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
