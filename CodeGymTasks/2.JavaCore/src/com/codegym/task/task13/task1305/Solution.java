d2ee2298dc54f8234182830f65af0527e0390d7c
Lom Thin <sarom_thin@outlook.com>
2019-09-24 22:31:19 -0700

    Completed task1306

--- CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1305/Solution.java
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1305/Solution.java
@@ -12,6 +12,35 @@ Requirements:
 5. Do not change interface declarations.
 */
 
+// Original Code
+//public class Solution {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println(new Dream().HOBBY.toString());
+//        System.out.println(new Hobby().toString());
+//
+//    }
+//
+//    interface Desire {
+//    }
+//
+//    interface Dream {
+//        private static Hobby HOBBY = new Hobby();
+//    }
+//
+//    class Hobby extends Desire implements Dream {
+//        static int INDEX = 1;
+//
+//        @Override
+//        public String toString() {
+//            INDEX++;
+//            return "" + INDEX;
+//        }
+//    }
+//
+//}
+
 public class Solution {
 
     public static void main(String[] args) {
