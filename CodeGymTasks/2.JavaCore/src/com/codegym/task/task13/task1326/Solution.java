3450f49eaa4877000caeb86e4050716817d8e74f
Lom Thin <sarom_thin@outlook.com>
2019-10-12 01:06:34 -0700

    Completed task 1326

--- CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1326/Solution.java
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1326/Solution.java
@@ -53,10 +53,12 @@ public class Solution {
             }
         }
 
+        readFile.close();
+
         for(int i = 1; i < list.size(); i++) {
-            int key = i;
+            int key = list.get(i);
             int j = i - 1;
-            while(j <= 0 && (key < list.get(j))) {
+            while(j >= 0 && (key < list.get(j))) {
                 list.set(j + 1, list.get(j));
                 j--;
             }
