b05290b99c5e68e974dc92ac605793171d466906
Lom Thin <sarom_thin@outlook.com>
2019-10-23 19:37:54 -0700

    Completed task1409

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task14/task1409/SuspensionBridge.java
@@ -0,0 +1,7 @@
+package com.codegym.task.task14.task1409;
+
+public class SuspensionBridge implements Bridge {
+    public int getCarsCount() {
+        return 10;
+    }
+}
---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task14/task1409/WaterBridge.java
@@ -0,0 +1,7 @@
+package com.codegym.task.task14.task1409;
+
+public class WaterBridge implements Bridge {
+    public int getCarsCount() {
+        return 1000000;
+    }
+}
