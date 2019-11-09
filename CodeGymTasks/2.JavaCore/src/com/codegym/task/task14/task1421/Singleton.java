85851114cf670e4e4f3f29026cb6d0d0b172d177
Lom Thin <sarom_thin@outlook.com>
2019-11-08 21:25:06 -0800

    Completed task1421

--- CodeGymTasks/2.JavaCore/src/com/codegym/task/task14/task1421/Singleton.java
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task14/task1421/Singleton.java
@@ -1,7 +1,16 @@
 package com.codegym.task.task14.task1421;
 
 public class Singleton {
-    public static Singleton getInstance() {
+    private static Singleton instance;
 
+    private Singleton() {
+        this.instance = this;
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
     }
 }
